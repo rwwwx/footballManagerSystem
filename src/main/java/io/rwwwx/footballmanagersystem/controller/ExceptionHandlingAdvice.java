41b05b77a8b897b39150630f92f8049e02b13261
rwwwx <vlad.matiukhin@gmail.com>
2022-05-15 21:57:15 +0300

    minor changes.

--- src/main/java/io/rwwwx/footballmanagersystem/controller/ExceptionHandlingAdvice.java
+++ src/main/java/io/rwwwx/footballmanagersystem/controller/ExceptionHandlingAdvice.java
@@ -1,5 +1,6 @@
 package io.rwwwx.footballmanagersystem.controller;
 
+import io.rwwwx.footballmanagersystem.exception.AccountException;
 import io.rwwwx.footballmanagersystem.exception.InvalidIdException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,7 +13,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionHandlingAdvice {
 
     @ExceptionHandler(InvalidIdException.class)
-    public ResponseEntity<String> handle(InvalidIdException e) {
+    public ResponseEntity<String> handleInvalidIdException(InvalidIdException e) {
+        log.error("handled an exception - ", e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccountException.class)
+    public ResponseEntity<String> handleAccountException(AccountException e) {
         log.error("handled an exception - ", e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
